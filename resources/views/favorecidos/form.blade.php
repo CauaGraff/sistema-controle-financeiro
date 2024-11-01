01ffd60ca9c2f472f8d32dddb17fde16d432dff0
CauaGraff <cauagraff@gmail.com>
2024-11-01 16:48:21 -0300

    fornecedores

--- resources/views/favorecidos/form.blade.php
+++ resources/views/favorecidos/form.blade.php
@@ -1,38 +1,47 @@
 <div class="mb-3">
     <label for="cnpj_cpf" class="form-label">CNPJ/CPF</label>
-    <input type="text" class="form-control" id="cnpj_cpf" name="cnpj_cpf" value="{{ old('cnpj_cpf', $favorecido->cnpj_cpf ?? '') }}" required>
+    <input type="text" class="form-control" id="cnpj_cpf" name="cnpj_cpf"
+        value="{{ old('cnpj_cpf', $favorecido->cnpj_cpf ?? '') }}" required>
 </div>
 <div class="mb-3">
     <label for="telefone" class="form-label">Telefone</label>
-    <input type="text" class="form-control" id="telefone" name="telefone" value="{{ old('telefone', $favorecido->telefone ?? '') }}">
+    <input type="text" class="form-control" id="telefone" name="telefone"
+        value="{{ old('telefone', $favorecido->telefone ?? '') }}">
 </div>
 <div class="mb-3">
     <label for="email" class="form-label">Email</label>
-    <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $favorecido->email ?? '') }}">
+    <input type="email" class="form-control" id="email" name="email"
+        value="{{ old('email', $favorecido->email ?? '') }}">
 </div>
 <div class="mb-3">
     <label for="cep" class="form-label">CEP</label>
-    <input type="text" class="form-control" id="cep" name="cep" value="{{ old('cep', $favorecido->cep ?? '') }}" required>
+    <input type="text" class="form-control" id="cep" name="cep" value="{{ old('cep', $favorecido->cep ?? '') }}"
+        required>
 </div>
 <div class="mb-3">
     <label for="uf" class="form-label">UF</label>
-    <input type="text" class="form-control" id="uf" name="uf" value="{{ old('uf', $favorecido->uf ?? '') }}" required maxlength="2">
+    <input type="text" class="form-control" id="uf" name="uf" value="{{ old('uf', $favorecido->uf ?? '') }}" required
+        maxlength="2">
 </div>
 <div class="mb-3">
     <label for="cidade" class="form-label">Cidade</label>
-    <input type="text" class="form-control" id="cidade" name="cidade" value="{{ old('cidade', $favorecido->cidade ?? '') }}" required>
+    <input type="text" class="form-control" id="cidade" name="cidade"
+        value="{{ old('cidade', $favorecido->cidade ?? '') }}" required>
 </div>
 <div class="mb-3">
     <label for="bairro" class="form-label">Bairro</label>
-    <input type="text" class="form-control" id="bairro" name="bairro" value="{{ old('bairro', $favorecido->bairro ?? '') }}" required>
+    <input type="text" class="form-control" id="bairro" name="bairro"
+        value="{{ old('bairro', $favorecido->bairro ?? '') }}" required>
 </div>
 <div class="mb-3">
     <label for="rua" class="form-label">Rua</label>
-    <input type="text" class="form-control" id="rua" name="rua" value="{{ old('rua', $favorecido->rua ?? '') }}" required>
+    <input type="text" class="form-control" id="rua" name="rua" value="{{ old('rua', $favorecido->rua ?? '') }}"
+        required>
 </div>
 <div class="mb-3">
     <label for="complemento" class="form-label">Complemento</label>
-    <input type="text" class="form-control" id="complemento" name="complemento" value="{{ old('complemento', $favorecido->complemento ?? '') }}">
+    <input type="text" class="form-control" id="complemento" name="complemento"
+        value="{{ old('complemento', $favorecido->complemento ?? '') }}">
 </div>
 <div class="mb-3">
     <label for="tipo" class="form-label">Tipo</label>
