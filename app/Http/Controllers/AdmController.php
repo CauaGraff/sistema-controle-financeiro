61dc75b06609cf836d9abfbc370c44a8ac49aca4
CauaGraff <cauagraff@gmail.com>
2025-04-24 09:31:01 -0300

    consulta por escritorio home adm

--- app/Http/Controllers/AdmController.php
+++ app/Http/Controllers/AdmController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Empresas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdmController extends Controller
 {
     public function index()
     {
-        $clientesAtivos = User::where('id_typeuser', 3)->where('active', 1)->count();
-        $empresasAtivas = Empresas::where('active', 1)->where('active', 1)->count();
-        $usuariosEscritorio = User::where('id_typeuser', 2)->where('active', 1)->count();
+        $clientesAtivos = User::where('id_typeuser', 3)->where('active', 1)->where('id_escritorio', Auth::user()->id_escritorio)->count();
+        $empresasAtivas = Empresas::where('active', 1)->where('active', 1)->where('id_escritorio', Auth::user()->id_escritorio)->count();
+        $usuariosEscritorio = User::where('id_typeuser', 2)->where('active', 1)->where('id_escritorio', Auth::user()->id_escritorio)->count();
 
         return view('admin.home', compact('clientesAtivos', 'empresasAtivas', 'usuariosEscritorio'));
     }
